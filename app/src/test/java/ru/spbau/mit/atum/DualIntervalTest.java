a52c963094bc8708746be5c576e79bf0e00be437
Ivan Veselov <veselov.iva@gmail.com>
2016-11-07 01:15:43 +0300

    Remove unnecessary javadocs.

--- app/src/test/java/ru/spbau/mit/atum/DualIntervalTest.java
+++ app/src/test/java/ru/spbau/mit/atum/DualIntervalTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class DualIntervalTest {
-    /**
-     * Проверяет работоспособность методов доступа к полям.
-     */
     @Test
     public void testGetters() throws Exception {
         DualInterval normalInterval = new DualInterval(1, 2, false);
--- app/src/test/java/ru/spbau/mit/atum/IntervalTest.java
+++ app/src/test/java/ru/spbau/mit/atum/IntervalTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class IntervalTest {
-    /**
-     * Проверяет работоспособность конструктора. Последний вызов должен выбросить исключение.
-     */
     @Test(expected = IllegalArgumentException.class)
     public void testConstructor() throws Exception {
         new Interval(1, 2);
@@ -17,9 +14,6 @@ public class IntervalTest {
         new Interval(2, 1);
     }
 
-    /**
-     * Проверяет работоспособность методов доступа к полям.
-     */
     @Test
     public void testGetters() throws Exception {
         Interval interval = new Interval(-1, 2);
