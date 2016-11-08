096e3388d0450746350fafe5636a0aad4c68ca5f
Ivan Veselov <veselov.iva@gmail.com>
2016-11-08 22:45:48 +0300

    Add WeekFilter class.

---
+++ app/src/main/java/ru/spbau/mit/atum/WeekFilter.java
@@ -0,0 +1,115 @@
+package ru.spbau.mit.atum;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Фильтр, который задает непрерывный промежуток времени для каждого из заданных дней недели.
+ * Промежуток может заканчиваться на следующий день, но его длительность не должна превышать суток.
+ *
+ * TODO: ограничение на длину промежутка можно убрать.
+ * Для этого потребуется усложнить алгоритм преобразования в список отрезков.
+ */
+public class WeekFilter extends TimeFilter {
+    private static final String INVALID_INTERVAL_MSG = "Invalid time interval for week filter.";
+
+    // TODO: стоило бы вынести эти константы в более подходящее место
+
+    private static final int minutesInDay = (int) TimeUnit.DAYS.toMinutes(1);
+
+    private static final int minutesInHour = (int) TimeUnit.HOURS.toMinutes(1);
+
+    private static final int daysInWeek = 7;
+
+    private int firstMinute;
+
+    private int duration;
+
+    private WeekMask mask;
+
+    /**
+     * Создает новый фильтр.
+     *
+     * @param firstMinute первая минута дня, которую задает фильтр.
+     * @param duration длина одного промежутка. Не должна превышать 24 часа.
+     * @param mask маска, представляющая те дни недели, в которых находятся временные промежутки,
+     *             которые задает фильтр. Неделя начинается с воскресенья.
+     * @param exclusiveFlag если true, то фильтр будет исключающим.
+     */
+    public WeekFilter(int firstMinute, int duration, @NotNull WeekMask mask,
+                      boolean exclusiveFlag) {
+        super(exclusiveFlag);
+
+        if (firstMinute < 0 || firstMinute >= minutesInDay
+                || duration < 0 || duration > minutesInDay) {
+            throw new IllegalArgumentException(INVALID_INTERVAL_MSG);
+        }
+
+        this.firstMinute = firstMinute;
+        this.duration = duration;
+        this.mask = mask;
+    }
+
+    /**
+     * @return первая минута дня, которую задает фильтр.
+     */
+    public int getFirstMinute() {
+        return firstMinute;
+    }
+
+    /**
+     * @return длина одного промежутка.
+     */
+    public int getDuration() {
+        return duration;
+    }
+
+    /**
+     * @return маска дней недели, которые задает фильтр.
+     */
+    public @NotNull WeekMask getWeekMask() {
+        return mask;
+    }
+
+    /**
+     * @param initialMoment точка отсчета и нижняя граница времени.
+     * @param globalInterval целочисленный интервал всего доступного времени.
+     * @return набор непересекающихся интервалов, представляющий временные промежутки, которые
+     *         задает фильтр.
+     */
+    @Override
+    protected List<DualInterval> intervalRepresentationImpl(@NotNull Calendar initialMoment,
+                                                            @NotNull Interval globalInterval) {
+        int minuteOffset = initialMoment.get(Calendar.HOUR_OF_DAY) * minutesInHour
+                         + initialMoment.get(Calendar.MINUTE);
+
+        // У воскресенье номер 1
+        int dayOfWeek = initialMoment.get(Calendar.DAY_OF_WEEK) - Calendar.SUNDAY;
+
+        int currentIntervalBeginning = firstMinute - minuteOffset;
+
+        List<DualInterval> intervalList = new ArrayList<>();
+
+        while (currentIntervalBeginning < globalInterval.end()) {
+           if (mask.isSet(dayOfWeek)) {
+                DualInterval interval = new DualInterval(currentIntervalBeginning,
+                        currentIntervalBeginning + duration,
+                        isExclusive());
+
+                interval.intersectWith(globalInterval);
+                if (!interval.isEmpty()) {
+                    intervalList.add(interval);
+                }
+            }
+
+            dayOfWeek = (dayOfWeek + 1) % daysInWeek;
+            currentIntervalBeginning += minutesInDay;
+        }
+
+        return intervalList;
+    }
+}
---
+++ app/src/test/java/ru/spbau/mit/atum/WeekFilterTest.java
@@ -0,0 +1,92 @@
+package ru.spbau.mit.atum;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static ru.spbau.mit.atum.TestUtilities.theNthOfJan;
+
+public class WeekFilterTest {
+    @Test
+    public void testConstructor1() throws Exception {
+        new WeekFilter(60, 10, new WeekMask(0, 1, 2), false);
+        new WeekFilter(60 * 23, 300, new WeekMask(3, 4, 5), false);
+        new WeekFilter(60 * 20, 101, new WeekMask(0, 1, 2, 3, 4, 5, 6), true);
+        new WeekFilter(65, 60 * 24, new WeekMask(0), false);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor2() throws Exception {
+        new WeekFilter(0, 60 * 25, new WeekMask(0), false);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor3() throws Exception {
+        new WeekFilter(0, -25, new WeekMask(0), false);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor4() throws Exception {
+        new WeekFilter(-1, 10, new WeekMask(0), false);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor5() throws Exception {
+        new WeekFilter(24 * 60, 10, new WeekMask(0), false);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor6() throws Exception {
+        new WeekFilter(30 * 60, 10, new WeekMask(0), false);
+    }
+
+    @Test
+    public void testGetters() throws Exception {
+        WeekFilter filter = new WeekFilter(10, 111, new WeekMask(0, 2, 3, 6), true);
+
+        assertEquals(10, filter.getFirstMinute());
+        assertEquals(111, filter.getDuration());
+        assertEquals(true, filter.isExclusive());
+
+        WeekMask mask = filter.getWeekMask();
+        assertEquals(true, mask.isSet(0));
+        assertEquals(false, mask.isSet(1));
+        assertEquals(true, mask.isSet(2));
+        assertEquals(true, mask.isSet(3));
+        assertEquals(false, mask.isSet(4));
+        assertEquals(false, mask.isSet(5));
+        assertEquals(true, mask.isSet(6));
+    }
+
+    private int[] dualIntervalsToArray(List<DualInterval> list) {
+        int[] array = new int[list.size() * 2];
+        for (int i = 0; i < list.size(); i++) {
+            array[2 * i] = list.get(i).begin();
+            array[2 * i + 1] = list.get(i).end();
+        }
+
+        return array;
+    }
+
+    @Test
+    public void testIntervalRepresentation() throws Exception {
+        int duration = 65;
+        WeekFilter filter = new WeekFilter(12 * 60, duration, new WeekMask(2, 6), false);
+
+        // вт(4, 13:00-13:05)/сб(8, 12:00-13:05)/вт(11, 12:00-13:05)/сб(15, 12:00-12:31)
+        List<DualInterval> list = filter.intervalRepresentation(theNthOfJan(4, 13, 0),
+                                                                theNthOfJan(15, 12, 31));
+
+        int the8 = (int) TimeUnit.DAYS.toMinutes(4) - 60;
+        int the11 = (int) TimeUnit.DAYS.toMinutes(7) - 60;
+        int the15 = (int) TimeUnit.DAYS.toMinutes(11) - 60;
+        assertArrayEquals(new int[] {0, 5,
+                                     the8, the8 + duration,
+                                     the11, the11 + duration,
+                                     the15, the15 + 31},
+                          dualIntervalsToArray(list));
+    }
+}
