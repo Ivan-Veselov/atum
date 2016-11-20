f008f4b753123fb45597c01ecaf2c52e6a8bb95f
tbelova <yukikomodo@gmail.com>
2016-11-20 19:26:06 +0300

    Move new task button from MainActivity to TaskListActivity. Add insertion of new task into task list.

--- app/src/main/java/ru/spbau/mit/atum/TaskListActivity.java
+++ app/src/main/java/ru/spbau/mit/atum/TaskListActivity.java
@@ -26,6 +26,13 @@ public class TaskListActivity extends AppCompatActivity {
     private ArrayList<Map<String, Object>> data;
     private Map<String, Object> m;
 
+    private final int DELETE_ID = 0;
+    private final int EDIT_ID = 1;
+
+    private final int NEW_TASK_CODE = 0;
+    private final int EDIT_TASK_CODE = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,15 +42,7 @@ public class TaskListActivity extends AppCompatActivity {
 
         data = new ArrayList<>();
         for (UserDefinedTask task: userDefinedTaskList) {
-            m = new HashMap<>();
-            m.put(TASK_NAME, task.getName());
-            if (task.getScheduledTime() == null) {
-                m.put(IS_SCHEDULED, "not scheduled");
-            } else {
-                m.put(IS_SCHEDULED, "OK");
-            }
-
-            data.add(m);
+            addNewTask(task);
         }
 
         String[] from = { TASK_NAME, IS_SCHEDULED };
@@ -57,8 +56,33 @@ public class TaskListActivity extends AppCompatActivity {
 
     }
 
-    private final int DELETE_ID = 0;
-    private final int EDIT_ID = 1;
+    public void onNewTaskClick(View view) {
+        Intent intent = new Intent(this, TaskEditorActivity.class);
+        startActivityForResult(intent, NEW_TASK_CODE);
+    }
+
+    private void addNewTask(UserDefinedTask task) {
+        m = new HashMap<>();
+        m.put(TASK_NAME, task.getName());
+        if (task.getScheduledTime() == null) {
+            m.put(IS_SCHEDULED, "not scheduled");
+        } else {
+            m.put(IS_SCHEDULED, "OK");
+        }
+
+        data.add(m);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == NEW_TASK_CODE && resultCode == RESULT_OK) {
+            UserDefinedTask newTask = (UserDefinedTask)data.getSerializableExtra("edit task");
+            userDefinedTaskList.add(newTask);
+            addNewTask(newTask);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
@@ -78,6 +102,9 @@ public class TaskListActivity extends AppCompatActivity {
             return true;
         }
         if (item.getItemId() == EDIT_ID) {
+            Intent intent = new Intent(this, TaskEditorActivity.class);
+            startActivityForResult(intent, EDIT_TASK_CODE);
+
             Toast.makeText(getApplicationContext(), "edit", Toast.LENGTH_LONG).show();
             adapter.notifyDataSetChanged();
             return true;
