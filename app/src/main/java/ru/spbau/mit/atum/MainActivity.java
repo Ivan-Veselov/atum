56994bff6c7086edd253d8247ef728fe8260bd28
tbelova <yukikomodo@gmail.com>
2016-12-17 22:32:36 +0300

    Remove some useless code.

--- app/src/main/java/ru/spbau/mit/atum/MainActivity.java
+++ app/src/main/java/ru/spbau/mit/atum/MainActivity.java
@@ -18,18 +18,13 @@ public class MainActivity extends UserDataEditorActivity {
         loadUserData();
     }
 
-    private final int TASK_CODE = 0;
-    private final int BLOCKER_CODE = 1;
-
     public void onTaskListClick(View view) {
         Intent intent = new Intent(this, TaskListActivity.class);
-        //intent.putExtra("filter holder type", TASK_CODE);
         startActivity(intent);
     }
 
     public void onBlockerListClick(View view) {
         Intent intent = new Intent(this, TimeBlockerListActivity.class);
-        //intent.putExtra("filter holder type", BLOCKER_CODE);
         startActivity(intent);
     }
 
